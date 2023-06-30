a3bf985d74cddef7a0e479dc9c7b17695d066e6e
Ulf FrostyPaw <19324157@uagro.mx>
2023-06-29 22:22:47 -0600

    Update

--- src/map.tsx
+++ src/map.tsx
@@ -84,7 +84,7 @@ const Map = () => {
 	return (
 		<>
 			<Suspense>
-				<MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false} style={{ height: "calc(100vh - 56px)" }}>
+				<MapContainer center={[21.2709003, -100.7189763]} zoom={5} scrollWheelZoom={false} style={{ height: "calc(100vh - 56px)" }}>
 					<TileLayer
 						attribution={'&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'}
 						url={"https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}.png"}
