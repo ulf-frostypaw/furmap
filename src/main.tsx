ac691f1fa111933ad3e0c6cac7641b4bda4789c1
Ulf FrostyPaw <ulffrostypaw@gmail.com>
2024-12-19 14:52:19 -0600

    feat(users): agregar perfil de usuario y error 404

--- src/main.tsx
+++ src/main.tsx
@@ -7,6 +7,8 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 // === [PAGES] ===
 import Map from "./pages/Map";
+import User from "./pages/profile/User";
+import Error404 from "./pages/Error404";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
@@ -15,6 +17,9 @@ createRoot(document.getElementById("root")!).render(
         <Routes>
           <Route path="/" element={<Navigate to={'/map'} />} />
           <Route path="/map" element={<Map />} />
+          <Route path="/profile/:username" element={<User />} />
+
+          <Route path="*" element={<Error404 />} />
         </Routes>
       </BrowserRouter>
     </HelmetProvider>
