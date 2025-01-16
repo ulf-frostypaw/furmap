8412adc3b92f651a2585bc4fccdfeb1cdcf9b881
Ulf FrostyPaw <ulffrostypaw@gmail.com>
2025-01-15 20:14:35 -0600

    feat(user profile): agregar perfil de usuario

---
+++ src/pages/settings/General.tsx
@@ -0,0 +1,13 @@
+import Layout from "../../components/Layout";
+
+const SettingsPage = () => {
+    return(
+        <Layout title="Settings">
+            <div className="container mx-auto my-4">
+                settings
+            </div>
+        </Layout>
+    );
+}
+
+export default SettingsPage
